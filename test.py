b13d3dbb6476dea2af2738f61d40aa768e7b3286
gabrielvmastery <72418015+gabrielvmastery@users.noreply.github.com>
2020-10-28 10:19:16 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
+from datetime import datetime
+import os
+file_path = <PASS YOUR FILE HERE>
+outputFile = 'ML_' + str(datetime.now().strftime('%Y%m%d_%H%M%S')) + '.csv'
+
 with open('H:\Results.csv','r') as openFile:
     with open('H:\Output.csv','w') as writeFile:
         for line in openFile:
             #split line by comma and compare first column, can be modified depending on given file            
             cleanline = line.strip('\n')
             column =  cleanline.split(',')
+
+
+
