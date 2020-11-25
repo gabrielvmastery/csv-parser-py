27302579a0fae5a5d75d9d9436025c6e8e9f88f6
gabrielvmastery <72418015+gabrielvmastery@users.noreply.github.com>
2020-11-24 23:09:34 -0600

    Update test.py

--- test.py
+++ test.py
@@ -4,16 +4,15 @@ import csv
 
 HEADERS = ['Load','CompletionTime']
 
-#outputFile = 'MasteryOutput_' + str(datetime.now().strftime('%Y%m%d_%H%M%S')) + '.csv'
-
 with open('LoadsTable.csv','r') as openFile:
-    with open('Output2.csv','w', newline='') as csvFile:
+    with open('mastery_' + str(datetime.now().strftime('%Y%m%d_%H%M%S')) + '.csv','w', newline='') as csvFile:
+#    with open('Output2.csv','w', newline='') as csvFile:
         for line in openFile:
             l = line.strip('\n')
             c =  l.split(',')
 
             #for each in c:
-            #    test for "" cases
+            #    if each == ": 
             writer = csv.writer(csvFile)
 #           writer.writerow(HEADERS)
             writer.writerow(c)
