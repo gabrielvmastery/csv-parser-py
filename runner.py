3ecc509a07dce288c3c08142cc3aeb605a469386
gabrielvmastery <72418015+gabrielvmastery@users.noreply.github.com>
2020-11-27 13:34:40 -0600

    Update and rename test.py to runner.py

--- runner.py
+++ runner.py
@@ -6,18 +6,16 @@ HEADERS = ['Load','CompletionTime']
 
 with open('LoadsTable.csv','r') as openFile:
     with open('mastery_' + str(datetime.now().strftime('%Y%m%d_%H%M%S')) + '.csv','w', newline='') as csvFile:
-#    with open('Output2.csv','w', newline='') as csvFile:
+
         for line in openFile:
             l = line.strip('\n')
             c =  l.split(',')
 
             #for each in c:
-            #    if each == ": 
+            #    if each == ":   #test for quotes in input line and handle accordingly 
             writer = csv.writer(csvFile)
             writer.writerow(c)
             print(c)
-
             #           writer.writerow(HEADERS)
-            
 openFile.close()
 csvFile.close()
