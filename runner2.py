f7adf6c0d582d3eed848aa1c4c02a5debb77a415
gabrielvmastery <72418015+gabrielvmastery@users.noreply.github.com>
2020-12-02 16:29:47 -0600

    Update runner2.py

--- runner2.py
+++ runner2.py
@@ -5,9 +5,9 @@ import csv
 HEADERS = ['Load','CompletionTime']
 
 with open('LoadsTable.csv','r') as openFile:
-    with open('mlogistics_' + str(datetime.now().strftime('%Y%m%d_%H%M%S')) + '.csv','w', newline='') as csvFile:
+    with open(str(datetime.now().strftime('%Y%m%d_%H%M%S')) + '_mlogistics_' + '.csv','w', newline='') as csvFile:
         #writer.writerow(HEADERS)
-        writer = csv.writer(csvFile, quotechar="'") #quotechar required for "" in columns (M)
+        writer = csv.writer(csvFile, quotechar="'")#quotechar required for "" in columns (M)
         for line in openFile:
             l = line.strip('\n')
             c =  l.split(',')
