5a262fcf8bd8637332889385e636831fb78f5d69
gabrielvmastery <72418015+gabrielvmastery@users.noreply.github.com>
2020-12-02 09:58:53 -0600

    Update runner2.py

--- runner2.py
+++ runner2.py
@@ -5,15 +5,12 @@ import csv
 HEADERS = ['Load','CompletionTime']
 
 with open('LoadsTable.csv','r') as openFile:
-    with open('mlogic-csv-parse' + str(datetime.now().strftime('%Y%m%d_%H%M%S')) + '.csv','w', newline='') as csvFile:
+    with open('mlogistics_' + str(datetime.now().strftime('%Y%m%d_%H%M%S')) + '.csv','w', newline='') as csvFile:
         #writer.writerow(HEADERS)
-        writer = csv.writer(csvFile)
+        writer = csv.writer(csvFile, quotechar="'") #quotechar required for "" in columns (M)
         for line in openFile:
             l = line.strip('\n')
             c =  l.split(',')
-
-            #for each in c:
-            #    if each == ":   #test for quotes in input line and handle accordingly
 
             writer.writerow(c)
 
