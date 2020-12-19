bdae21a55be36f279c114b1dbf0494a692736fb7
gabrielvmastery <72418015+gabrielvmastery@users.noreply.github.com>
2020-12-18 19:22:05 -0600

    Update check-for-dup-orders.py

--- check-for-dup-orders.py
+++ check-for-dup-orders.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 import os
 import csv
-
+HEADERS = ['ORDER_ID','ORDER_NUMBER']
 with open('all-mm-orders.csv','r') as openFile:
     with open(str(datetime.now().strftime('%Y%m%d_%H%M%S')) + '_mlogistics_' + '.csv','w', newline='') as csvFile:
         writer = csv.writer(csvFile, quotechar="'")#quotechar required for "" in columns (M)
+        writer.writerow(HEADERS)
         seen = set()
         for line in openFile:
             if line in seen:
